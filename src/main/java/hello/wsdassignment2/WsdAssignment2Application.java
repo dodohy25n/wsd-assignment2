2ae0abba85def1589c7e3312067fe620efcd58cf
dohyeon <ehgus8910@gmail.com>
2025-12-04 18:11:34 +0900

    first commit

---
+++ src/main/java/hello/wsdassignment2/WsdAssignment2Application.java
@@ -0,0 +1,13 @@
+package hello.wsdassignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdAssignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdAssignment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/wsdassignment2/WsdAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.wsdassignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdAssignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
